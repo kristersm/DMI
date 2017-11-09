fd499749aa7e3c8495aecfcefa702400b2778b3d
user <kristers.misa@inbox.lv>
2017-11-09 17:36:19 +0200

    20171109_17_35

---
+++ PYTHON/dialogs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+a = raw_input ("Lietotāj, lūdzu ievadi savu vārdu:")
+b = raw_input ("Lietotāj, lūdzu ievadi savu uzvārdu:")
+print "Tātad, tevi sauc - %s"%(a + ' ' +  b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+a = input("Lietotāj, lūdzu ievadi kko:")
+print "Tu esi ievadījis %s tipa datus"%(type(a))
+print a * a
+print a + a
+a = raw_input("Lietotāj, lūdzu ievadi kko:")
+print "Tu esi ievadījis %s tipa datus"%(type(a))
+#print a * a
+print a + a
+'''
---
+++ PYTHON/hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+print "Hello World!"
+'''
+#http://www.tutorialspoint.com/unix_commands/echo.htm
+# Example 3. - 7.
+# example 3
+print  "Here \bthe \bspaces \bare \bbackspaced."
+# exapmple 4
+print  "Here \nthe \nspaces \nare \nnewlined."
+# example 5
+print  "Here \tthe \tspaces \thave \thorizontal \ttab \tspaces."
+# example 6
+print  "Here \vthe \vspaces \vhave \vvertical \vtab \vspaces."
+# example 7
+print  "Here the \rcontent before this is removed."  
