fc3ef884f985f074fb01052edd1dd9a6bffeaa8e
user <kristers.misa@inbox.lv>
2017-11-16 16:03:52 +0200

    20171116_16_05

---
+++ PYTHON/sin_caur_summu_ver2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# a0, a1, a2, a3
+# s0, s1, s2, s3 -> s
+from math import sin
+x = 1. *  input("Lietotāj, lūdzu ievadi argumentu (x): ")
+print type(x)
+y = sin(x)
+print "sin(%.2f)=%.2f"%(x,y)
+
+a0 = (-1)**0*x**1/(1)
+#s0 = a0
+s = a0
+print "a0 = %6.2f S0 = %6.2f"%(a0,s)
+
+a1 = (-1)**1*x**3/(1*2*3) # (-1)**1*(2)**3/6=-8/6=-1
+#s1 = a0 + a1
+#s1 = s0 + a1
+s = s + a1
+print "a1 = %6.2f S1 = %6.2f"%(a1,s)
+
+a2 = (-1)**2*x**5/(1*2*3*4*5)
+#s2 = a0 + a1 + a2
+#s2 = s1 + a2
+s= s + a2
+print "a2 = %6.2f S2 = %6.2f"%(a2,s)
+
+a3 = (-1)**3*x**7/(1*2*3*4*5*6*7)
+#s3 = a0 + a1 + a2 + a3
+#s3 = s2 + a3
+s = s + a3
+print "a3 = %6.2f S3 = %6.2f"%(a3,s)
